d0b209317fdf0cc120e92e3d33b12e908ed3bfbf
Marc Neudert <marc.neudert@gmail.com>
2018-10-14 15:09:31 +0200

    wires up ua_inspector call

--- lib/ua_inspector/plug.ex
+++ lib/ua_inspector/plug.ex
@@ -3,9 +3,25 @@ defmodule UAInspector.Plug do
   UAInspector Plug
   """
 
+  import Plug.Conn
+
   @behaviour Plug
 
   def init(opts), do: opts
 
-  def call(conn, _opts), do: conn
+  def call(conn, _opts) do
+    lookup =
+      case get_req_header(conn, "user-agent") do
+        [] -> UAInspector.parse(nil)
+        [agent | _] -> UAInspector.parse(agent)
+      end
+
+    put_private(conn, :ua_inspector, lookup)
+  end
+
+  @doc """
+  Returns the lookup result from the connection.
+  """
+  @spec get_result(Plug.Conn.t()) :: nil | UAInspector.Result.t()
+  def get_result(conn), do: conn.private[:ua_inspector]
 end
--- test/ua_inspector/plug_test.exs
+++ test/ua_inspector/plug_test.exs
@@ -2,6 +2,8 @@ defmodule UAInspector.PlugTest do
   use ExUnit.Case
   use Plug.Test
 
+  @agent "Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53"
+
   defmodule Router do
     use Plug.Router
 
@@ -15,7 +17,26 @@ defmodule UAInspector.PlugTest do
 
   @opts Router.init([])
 
-  test "returns 200" do
-    assert 200 == conn(:get, "/") |> Router.call(@opts) |> Map.fetch!(:status)
+  test "empty result for empty agent" do
+    conn = conn(:get, "/") |> Router.call(@opts)
+
+    assert 200 == conn.status
+    assert %UAInspector.Result{user_agent: nil} == UAInspector.Plug.get_result(conn)
+  end
+
+  test "result for agent" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("user-agent", @agent)
+      |> Router.call(@opts)
+
+    assert 200 == conn.status
+
+    assert %UAInspector.Result{
+             client: %UAInspector.Result.Client{},
+             device: %UAInspector.Result.Device{},
+             os: %UAInspector.Result.OS{},
+             user_agent: @agent
+           } = UAInspector.Plug.get_result(conn)
   end
 end
