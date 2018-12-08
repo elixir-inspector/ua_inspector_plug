f89418cfc15e7c9cb16363376db09c1c86d4ddd7
Marc Neudert <marc.neudert@gmail.com>
2018-12-08 12:08:22 +0100

    improves path configurations

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,5 @@
 use Mix.Config
 
 if Mix.env() == :test do
-  config :ua_inspector,
-    database_path: Path.join(__DIR__, "../test/database")
+  config :ua_inspector, database_path: Path.expand("../test/database", __DIR__)
 end
