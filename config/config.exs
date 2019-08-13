6cb084aed8d7f7321ce45bab8e4e9088a8c9e1a3
Marc Neudert <marc.neudert@gmail.com>
2019-08-13 20:40:01 +0200

    Use synchronous startup for testing

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
 if Mix.env() == :test do
-  config :ua_inspector, database_path: Path.expand("../test/database", __DIR__)
+  config :ua_inspector,
+    database_path: Path.expand("../test/database", __DIR__),
+    startup_sync: true
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,1 @@
-alias UAInspector.Plug.TestHelpers.Retry
-
-true = Retry.retry(5000, 10, &UAInspector.ready?/0, & &1)
-
 ExUnit.start()
