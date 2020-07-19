d7b677dfafc370947ac4356a1c7ef912ab17d0bf
Marc Neudert <marc.neudert@gmail.com>
2020-07-19 12:34:24 +0200

    Improve development dependency constraints

--- mix.exs
+++ mix.exs
@@ -26,9 +26,9 @@ defmodule UAInspector.Plug.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.13", only: :test, runtime: false},
+      {:excoveralls, "~> 0.13.0", only: :test, runtime: false},
       {:plug, "~> 1.0"},
       {:ua_inspector, "~> 2.0"}
     ]
