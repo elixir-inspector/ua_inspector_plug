f9b3a4cc13da3e824621806b8a060c762845cec1
Marc Neudert <marc.neudert@gmail.com>
2023-11-07 12:20:35 +0100

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -28,9 +28,9 @@ defmodule UAInspector.Plug.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.3", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.17.0", only: :test, runtime: false},
+      {:excoveralls, "~> 0.18.0", only: :test, runtime: false},
       {:plug, "~> 1.0"},
       {:ua_inspector, "~> 3.1"}
     ]
